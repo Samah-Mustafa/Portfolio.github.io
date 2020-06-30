434440086e2265c0e2f4ff216ec431886fae9ba5
samah_mustafa <i.samaaah@gmail.com>
2020-06-30 23:41:06 +0300

    Second CHanges

--- src/Folio.js
+++ src/Folio.js
@@ -1,13 +1,133 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import img from "./assets/boatImage.jpg";
+import styled from 'styled-components';
+
+const Styles = styled.div`
+.your-dialog-classname {
+    animation-name: breath-animation;
+    animation-duration: 8s;
+    animation-iteration-count: infinite;
+}
+`;
 
 export const Folio = () => {
-    return (  
-      <div style={{width: "550px", margin: "auto", marginTop: "70px"}}>
-      <h1>Hello World!</h1>
-      <p>My name is Samah Mustafa. I'm a graduate from Faculty of Art and Design (field of specialization: Graphic Design and Advertising).
-      I’m passionate about creating and producing high-quality work. I'm dedicated to create designs that attract the eye especialy in Brand identity and Illustration.
-      I found it interested to generate new ideas to get as close as possible to the target.
-      One of my favourite challenges is to draft ideas that fit the brief or help bringing a creative angle if the client doesn’t entirely know what he’s after till I make sure that work is done.</p>
-  </div>
-    )
+
+
+  return (
+    <Styles>
+    <div className = "your-dialog-classname" style = {{ display:"flex", width: "100%", padding: "60px 30px", margin: "0 auto",}}>
+    <Card style = {{ display:"flex", width: "85%", textAlign: "center", padding: "10px 0", margin: "0 auto",}}>
+      <div>
+      <CardActionArea style = {{padding: "20px"}}>
+        <CardMedia style = {{ height: "300px", padding: '0'}}
+          component="img"
+          alt="Contemplative Reptile"
+          height="300"
+          image="https://i.ibb.co/mHbf8ZG/Others.jpg"
+          src="/src/assets/boatImage.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+      </div>
+
+      <div>
+      <CardActionArea style = {{padding: "20px"}}>
+        <CardMedia style = {{ height: "300px", paddingTop: '0'}}
+          component="img"
+          alt="Contemplative Reptile"
+          height="300"
+          image="https://i.ibb.co/wpjqJ8L/Vegetables.jpg"
+          src="/src/assets/photo.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+      </div>
+      
+      <div>
+      <CardActionArea style = {{padding: "20px"}}>
+        <CardMedia style = {{ height: "300px", paddingTop: '0'}}
+          component="img"
+          alt="Contemplative Reptile"
+          height="300"
+          image="https://i.ibb.co/PjYW59G/Sweets.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+      </div>
+    </Card>
+    </div>
+    </Styles>
+
+    
+  );
 }
+
+
+// <Card className={classes.root} style = {{ display:"flex", }}>
+// <CardMedia style = {{ height: "300px", padding: '0'}}
+// component="img"
+// alt="Contemplative Reptile"
+// height="300"
+// image={img}
+// src="/src/assets/boatImage.jpg"
+// title="Contemplative Reptile"
+// />
