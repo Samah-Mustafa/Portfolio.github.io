3e48f1ccdfe44e7e9b771cb49878f20b5e24d482
samah_mustafa <i.samaaah@gmail.com>
2020-07-23 02:37:15 +0300

    Updates

---
+++ precache-manifest.21e680f33394cf787bb3c651ba44373d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad0af3c9a5b9482fa747ea9f35214a1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e4e07e06c6a7c423c880",
+    "url": "/portfolio/static/css/main.f8c260f9.chunk.css"
+  },
+  {
+    "revision": "f5646e0e6aa66c072a76",
+    "url": "/portfolio/static/js/2.892dbae9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.892dbae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e07e06c6a7c423c880",
+    "url": "/portfolio/static/js/main.eeb3f793.chunk.js"
+  },
+  {
+    "revision": "5d96ae9e392a39d29dcf",
+    "url": "/portfolio/static/js/runtime-main.a077f93d.js"
+  }
+]);
