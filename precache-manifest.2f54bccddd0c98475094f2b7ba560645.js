520d96f8d0610df078c5814c15cba0eb6ab642fb
samah_mustafa <i.samaaah@gmail.com>
2020-07-23 05:55:02 +0300

    Updates

--- precache-manifest.2f54bccddd0c98475094f2b7ba560645.js
+++ precache-manifest.2f54bccddd0c98475094f2b7ba560645.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "544cfaa346bb0a477f489a5c264aeb12",
+    "revision": "cc997e05d79c771896c616fd01cb6567",
     "url": "/Portfolio/index.html"
   },
   {
-    "revision": "3a32b22a011948aad283",
+    "revision": "9e6e9ba8cc687a18e4b1",
     "url": "/Portfolio/static/css/main.033f40e6.chunk.css"
   },
   {
-    "revision": "3135fe4274e77dc5b104",
-    "url": "/Portfolio/static/js/2.28b7ad6a.chunk.js"
+    "revision": "6b5ea4c364aebe586c40",
+    "url": "/Portfolio/static/js/2.38dc79e4.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/Portfolio/static/js/2.28b7ad6a.chunk.js.LICENSE.txt"
+    "url": "/Portfolio/static/js/2.38dc79e4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3a32b22a011948aad283",
-    "url": "/Portfolio/static/js/main.b8758529.chunk.js"
+    "revision": "9e6e9ba8cc687a18e4b1",
+    "url": "/Portfolio/static/js/main.ec260e85.chunk.js"
   },
   {
     "revision": "2122ba40864d94d0ccc3",
